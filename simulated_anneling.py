0ea132ec0c8eeb2a9f2ee9bf6bbd83eb7352d516
nathhje <n.van.sburg@hotmail.nl>
2018-12-04 17:48:10 +0100

    added functions

--- simulated_anneling.py
+++ simulated_anneling.py
@@ -10,6 +10,41 @@ def importCities(filename):
 
     return cities
 
+def cool1(T):
+    alpha =0.99
+    return alpha*T
+
+def accept(cost1,cost2,T):
+    
+    if cost1<cost2:
+        return True
+    
+    chance = np.exp((cost1-cost2)/T)
+    
+    if random.random()<chance:
+        return True
+    
+    return False
+
+def costCalc(cities):
+    cost=0
+    
+    for i in range(len(cities)):
+        
+        cost+= ((cities[i][1]-cities[(i+1)%len(cities)][1])**2 (cities[i][2]-cities[(i+1)%len(cities)][2])**2)**0.5
+        
+    return cost
+
+def swap1(cities):
+    
+    index1 = random.randn(len(cities))
+    index2 = random.randn(len(cities))
+    
+    while index1 == index2:
+        index2 = random.randn(len(cities))
+        
+    cities[index1],cities[index2] = cities[index2],cities[index1]
+
 
 importCities("TSP-Configurations/eil51.tsp.txt")
 
