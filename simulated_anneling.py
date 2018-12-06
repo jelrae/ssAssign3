cd3c0005221f348cb8a792e9590cf725939f5ee4
nathhje <n.van.sburg@hotmail.nl>
2018-12-06 15:45:58 +0100

    swapt 2, not working
    added swap2 and slightly changed the cost function. however, the code says everything is intersecting, so there is a problem in if statement in intersection()

--- simulated_anneling.py
+++ simulated_anneling.py
@@ -10,7 +10,6 @@ def importOptimumPath(filename, coordinates):
     path = np.genfromtxt(filename, dtype = int, skip_header=5, delimiter = ' ', skip_footer = 1)
     pathAll = []
     
-    print(path)
     for i in range(len(path)):
         for j in range(len(coordinates)):
             
@@ -19,7 +18,6 @@ def importOptimumPath(filename, coordinates):
                 break
             
     pathAll = np.array(pathAll)
-    print(pathAll)
     return pathAll
 
 def importCities(filename):
@@ -46,19 +44,22 @@ def accept(cost1,cost2,T):
     chance = np.exp((cost2-cost1)/T)
     #print(chance)
     if random.random()<chance:
+        #print("accepted")
         return True
     
     return False
 
-def costCalc(cities):
+def costCalc(cities,costs):
     # initial cost calc function
     cost=0
     
-    for i in range(len(cities)):
+    for i in range(len(cities)-1):
 
-        cost+= ((cities[i][1]-cities[(i+1)%len(cities)][1])**2 + (cities[i][2]-cities[(i+1)%len(cities)][2])**2)**0.5
+        cost+= ((cities[i][1]-cities[i+1][1])**2 + (cities[i][2]-cities[i+1][2])**2)**0.5
     
-    return cost
+    costs[0].append(cost)
+    costs[1].append(len(costs[0]))
+    return cost,costs
 
 def swap1(cities):
     
@@ -73,24 +74,65 @@ def swap1(cities):
 
     return cities
 
-def simulated_anneling(path,Tstart):
+def intersection(cities):
+    
+    # Creates a list of all intersection in route
+    
+    number = len(cities)
+    intersections = []
+    
+    for i in range(number):
+        
+        i2 = (i+1)%number
+        i3 = (i+2)%number
+        i4 = (i+3)%number
+        
+        diff1 = [abs(cities[i][1]-cities[i2][1]),abs(cities[i][2]-cities[i2][2])]
+        diff2 = [abs(cities[i3][1]-cities[i4][1]),abs(cities[i3][2]-cities[i4][2])]
+        #print(diff1[0]*diff2[1] - diff2[0]*diff1[1])
+        if diff1[0]*diff2[1] - diff2[0]*diff1[1] != 0:
+            #print(diff1[0]*diff2[1] - diff2[0]*diff1[1])
+            intersections.append(i)
+            
+    return intersections
+
+def swap2(cities):
+    
+    number = len(cities)
+    
+    intersections = intersection(cities)
+    #print(len(intersections))
+    if len(intersections) == 0:
+        exit()
+    
+    index = (np.random.choice(intersections)+1)%number
+    index2 = (index+2)%number
+    
+    cities[index:index2] = np.flip(cities[index:index2])
+    
+    return cities
+        
+
+def simulated_annealing(path,Tstart,costs):
     #Initial simulated anneling fuction
     T = Tstart
     i = 0
-    pathcost = costCalc(path)
-    print(pathcost)
-    while T>17:
+    pathcost,costs = costCalc(path,costs)
+    #print(pathcost)
+    for i in range(1):#while T>0.0001:
         newpath = swap1(path)
-        newcost = costCalc(newpath)
+        newcost,costs = costCalc(newpath,costs)
+        i+=1
+        if i%20 == 0:
+            T = cool1(T)
+            print(T)
         if accept(newcost, pathcost, T):
-            i+=1
+            
             path = newpath
             pathcost = newcost
-            if i%20 == 0:
-                T = cool1(T)
                 #print(T)
 
-    return pathcost, path
+    return pathcost, path, costs
 
 def plotRoute(cities):
     
@@ -104,6 +146,12 @@ def plotRoute(cities):
     plt.figure()
     plt.plot(xlist,ylist)
     plt.show()
+    
+def plotCosts(costs):
+    
+    plt.figure()
+    plt.plot(costs[1],costs[0])
+    plt.figure()
 
 def main():
 
@@ -111,19 +159,21 @@ def main():
     optpath = importOptimumPath("TSP-Configurations/eil51.opt.tour.txt",init_path)
 
     plotRoute(init_path)
-    optcost = costCalc(optpath)
+    optlist = [[],[]]
+    optcost, optlist = costCalc(optpath,optlist)
     print(optcost)
     plotRoute(optpath)
     
-    annealedcost, annealedpath = simulated_anneling(init_path, 1000)
+    costs = [[],[]]
+    
+    annealedcost, annealedpath, costs = simulated_annealing(init_path, 1,costs)
 
     #print(optcost)
     print(annealedcost)
     print(annealedpath)
-    
     plotRoute(init_path)
     plotRoute(annealedpath)
-    plotRoute(optpath)
+    plotCosts(costs)
 
 if __name__ == "__main__":
     main()
